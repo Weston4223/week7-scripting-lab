5b66129ef33296ceb59812b553da9419d460a539
Weston Wagenr <weston.wagner@yahoo.com>
2026-03-16 11:53:46 -0400

    Added user input calcuation script

---
+++ script.py
@@ -0,0 +1,12 @@
+# Script that accepts user input and performs a calculation
+
+name = input("Enter your name: ")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_result = num1 + num2
+
+print("\n----- Result -----")
+print(f"Hello {name}!")
+print(f"The sum of {num1} and {num2} is {sum_result}")
+print("------------------")
