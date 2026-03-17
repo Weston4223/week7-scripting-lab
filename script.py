d22e55b80ff04c00171b3f686720233f5da4661f
Weston Wagenr <weston.wagner@yahoo.com>
2026-03-17 04:03:25 -0400

    Added input validation

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 # Script that accepts user input and performs a calculation
-
 name = input("Enter your name: ")
+
+if name == "":
+    print("Name cannot be empty")
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
